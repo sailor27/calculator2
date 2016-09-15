682a0e8079c041a1a5a6924cc7ef4ec2f765ec00
jin camou and sailor winkelman <jin@>
2016-09-14 19:22:07 -0700

    Add form in html for user input for add function. Modify back end logic to reference front end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,34 @@
-/*add function*/
-
+// Business (or back-end) logic:
 var add = function(number1, number2) {
 	return number1 + number2;
 };
-
-var number1 = parseInt(prompt("enter a number to add"));
-var number2 = parseInt(prompt("enter another number to add"));
-
-var result = add(number1, number2);
-
-/* subtract function */
 
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var number1 = parseInt(prompt("enter a number to subtract"));
-var number2 = parseInt(prompt("enter another number to subtract"));
-
-
-
-
-/*multiply function */
-
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+// Front-end logic:
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+	  var number1 = parseInt($("#add1").val());
+	  var number2 = parseInt($("#add2").val());
+	  alert(add(number1, number2));
+	});
+});
+
+/*var number1 = parseInt(prompt("enter a number to subtract"));
+var number2 = parseInt(prompt("enter another number to subtract"));
+
+var result = subtract(number1, number2);
+
 
 var number1 = parseInt(prompt("enter a number to multiply"));
 var number2 = parseInt(prompt("enter another number to multiply"));
@@ -33,13 +36,10 @@ var number2 = parseInt(prompt("enter another number to multiply"));
 var result = multiply(number1, number2);
 
 
-/*divide function */
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
+
 
 var number1 = parseInt(prompt("enter a number to divide"));
 var number2 = parseInt(prompt("enter another number to divide"));
 
 var result = divide(number1, number2);
-alert(result);
+*/
