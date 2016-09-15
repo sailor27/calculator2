0aa47434c99da517fccdc0eb22baa6fe0a05235a
jin camou and sailor winkelman <sailor@>
2016-09-14 19:46:04 -0700

    add form in html for add function in user input. modify back end logic to reference front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,31 @@
 var add = function(number1, number2) {
 	return number1 + number2;
 };
-
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
-
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
-
 var divide = function(number1, number2) {
   return number1 / number2;
 };
+
 // Front-end logic:
 $(document).ready(function() {
 	$("form#add").submit(function(event) {
 		event.preventDefault();
 	  var number1 = parseInt($("#add1").val());
 	  var number2 = parseInt($("#add2").val());
-	  alert(add(number1, number2));
+		var result = add(number1, number2);
+	  $("#output").text(result);
+	});
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+	  var number1 = parseInt($("#subtract1").val());
+	  var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+	  $("#output2").text(result);
 	});
 });
 
